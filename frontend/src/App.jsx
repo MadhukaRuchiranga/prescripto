813837c76973e8a8daa9aa7794833571e44502e8
Madhuka Ruchiranga Jayasinha <madhukaruchiranga2019@gmail.com>
2026-03-02 10:49:52 +0530

    Navbar implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,11 @@ import Contact from "./pages/Contact";
 import MyProfile from "./pages/MyProfile";
 import MyAppoinments from "./pages/MyAppoinments";
 import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/doctors" element={<Doctors />} />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>HomePage</div>;
+  return <div></div>;
 };
 
 export default Home;
