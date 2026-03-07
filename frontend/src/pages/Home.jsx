2358ac158990177e1722e85ac6aaced124d66c54
Madhuka Ruchiranga Jayasinha <madhukaruchiranga2019@gmail.com>
2026-03-07 16:09:37 +0530

    Top Doctors Initial UI Implementation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
 import Speciality from "../components/Speciality";
+import TopDoctors from "../components/TopDoctors";
 const Home = () => {
   return (
     <div>
       <Header />
       <Speciality />
+      <TopDoctors />
     </div>
   );
 };
